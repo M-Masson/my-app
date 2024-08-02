6734182675456be638d470cb748bd66d1b907ac1
Masson <morgan.masson006@gmail.com>
2024-08-02 17:49:08 +0200

    correction page error

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -6,7 +6,7 @@ function Error(){
             
             <h1>404</h1>
             <p>Oups! La page que vous demandez n'existe pas.</p>
-            <Link to="/">Retourner sur la page d'accueil</Link>
+            <Link to="/home">Retourner sur la page d'accueil</Link>
         </div>
     )
 }
